b68d6cd007f172ed36bddff9eb935bfb9bbb0075
don <misurakalu@gmail.com>
2023-08-31 23:12:46 +1000

    add video url

--- site4/src/components/cta/CTA.jsx
+++ site4/src/components/cta/CTA.jsx
@@ -10,6 +10,19 @@ const CTA = () => (
     <div className="gpt3__cta-btn">
       <button type="button">Get Started</button>
     </div>
+    {/* This next div has approirate stylind and has an embedded url
+    of  https://www.youtube.com/watch?v=0My9AGbp1rw&ab_channel=StackedDeckJiuJitsu
+    and starts playing the video as the person scrolls past with no audio */}
+    <div className="gpt3__cta-video">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/0My9AGbp1rw?autoplay=1&mute=1&loop=1&playlist=0My9AGbp1rw"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      />
+    </div>
   </div>
 );
 
