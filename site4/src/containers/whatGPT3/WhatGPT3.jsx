674bbf1250de0d78c690f6e2d8db073e10e79946
don <misurakalu@gmail.com>
2023-09-11 06:41:06 +1000

    still working on timetable

--- site4/src/containers/whatGPT3/WhatGPT3.jsx
+++ site4/src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,19 +2,102 @@ import React from 'react';
 import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
+// Define the timetable data as a two-dimensional array
+const timetableData = [
+  // Monday
+  [
+    { time: '06:00 AM - 07:00 AM', text: 'BJJ - Open Class' },
+    { time: '12:00 PM - 01:30 PM', text: 'BJJ - Open Class' },
+    { time: '04:00 PM - 05:00 PM', text: 'Kids Class' },
+    { time: '05:00 PM - 06:00 PM', text: 'Junior Class' },
+    { time: '06:00 PM - 07:30 PM', text: 'BJJ - Open Class' },
+    { time: '07:30 PM - 08:00 PM', text: 'BJJ - WB Class' },
+    // Add more time slots for Monday as needed
+  ],
+  // Tuesday
+  [
+    { time: '5:00 PM - 6:30 PM', text: 'BJJ Rounds' },
+    { time: '11:00 AM - 12:00 PM', text: 'MMA Striking' },
+    // Add more time slots for Tuesday as needed
+  ],
+  // Wednesday
+  [
+    { time: '06:00 AM - 07:00 AM', text: 'BJJ - Open Class' },
+    { time: '11:00 AM - 12:00 PM', text: 'MMA Striking' },
+    { time: '12:00 PM - 01:30 PM', text: 'BJJ - Open Class' },
+    { time: '04:00 PM - 05:00 PM', text: 'Kids Class' },
+    { time: '05:00 PM - 06:00 PM', text: 'Junior Class' },
+    { time: '06:00 PM - 07:30 PM', text: 'BJJ - Open Class' },
+    { time: '07:30 PM - 08:00 PM', text: 'BJJ - WB Class' },
+    // Add more time slots for Wednesday as needed
+
+  ],
+  // Thursday
+  [
+    { time: '5:00 PM - 6:30 PM', text: 'BJJ Rounds' },
+    { time: '11:00 AM - 12:00 PM', text: 'MMA Striking' },
+    // Add more time slots for Thursday as needed
+  ],
+  // Friday
+  [
+    { time: '06:00 AM - 07:00 AM', text: 'BJJ - Open Class' },
+    { time: '11:00 AM - 12:00 PM', text: 'MMA Striking' },
+    { time: '12:00 PM - 01:30 PM', text: 'BJJ - Open Class' },
+    { time: '04:00 PM - 05:00 PM', text: 'Kids Class' },
+    { time: '05:00 PM - 06:00 PM', text: 'Junior Class' },
+    { time: '06:00 PM - 07:30 PM', text: 'BJJ - Open Class' },
+    { time: '07:30 PM - 08:00 PM', text: 'BJJ - WB Class' },
+    // Add more time slots for Friday as needed
+  ],
+  // Saturday
+  [
+    { time: '04:00 PM - 05:00 PM', text: 'Kids Class' },
+    { time: '05:00 PM - 06:00 PM', text: 'Junior Class' },
+    { time: '06:00 PM - 07:30 PM', text: 'BJJ - Open Class' },
+    { time: '11:00 AM - 12:00 PM', text: 'MMA Striking' },
+    // Add more time slots for Saturday as needed
+  ],
+  // Sunday
+  [
+    { time: '06:00 AM - 07:30 PM', text: 'NO CLASS' },
+    // Add more time slots for Sunday as needed
+  ],
+];
+
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature title="Timetable" />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
       <p>Explore the Library</p>
     </div>
     <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <table className="timetable">
+        <thead>
+          <tr>
+            <th>Time</th>
+            {daysOfWeek.map((day, index) => (
+              <th key={index}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {timetableData[0].map((_, slotIndex) => (
+            <tr key={slotIndex}>
+              <td>{timetableData[0][slotIndex].time}</td>
+              {timetableData.map((dayData, dayIndex) => (
+                <td key={dayIndex}>
+                  {dayData[slotIndex] ? dayData[slotIndex].text : ''}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   </div>
 );
